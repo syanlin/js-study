2e7ff9319d3775b17e31524b2d080d3a6a9d6c5d
yanlin.shao <yanlin.shao@outlook.com>
2015-09-15 22:23:35 +0800

    修改changestyle相关的代码-toxing

--- js/changestyle.js
+++ js/changestyle.js
@@ -1,33 +1,41 @@
 /**
  * Created by 艳林 on 2015/9/14.
  */
-//改变颜色函数
+//改变颜色
  function changecolor()
-{
-    mychar = document.style.color;
-    mychar = document.style.backgroundColor;
+{   var yl = document.getElementById("xing");
+    yl.style.color = "red";
+    yl.style.backgroundColor = "blue";
 }
-
+//改变高低
 function changewidth()
 {
-    mychar = document.style.width;
-    mychar = document.style.height;
+    var yl = document.getElementById("xing");
+    yl.style.width = "300px";
+    yl.style.height = "500px";
 }
+//显示内容
 function display()
 {
-    mychar = document.style.display = none;
+    var yl = document.getElementById("xing");
+    yl.style.display = "block";
 }
-
+//隐藏内容
 function hide()
 {
-    mychar = document.style.display = none
+    var yl = document.getElementById("xing");
+    yl.style.display = "none";
 }
-
+//取消设置
 function cancel()
 {
-    mychar = confirm("是否取消设置")
-    if (mychar == true)
+    var yl = document.getElementById("xing")
+    var xx = confirm("是否取消设置");
+    if (xx == true)
     {
-
+        xing.removeAttribute("style");
+    }
+    else
+    {
     }
 }
