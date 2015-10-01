e4622bb469ace98e907c53d5d0358ce6bde6afb6
yanlin.shao <yanlin.shao@outlook.com>
2015-10-01 22:55:08 +0800

    添加page代码

--- js/max.js
+++ js/max.js
@@ -4,11 +4,12 @@
 function max(a, b)
 {
     if (a > b){
-        return a;}
-    else if (a == b){
-        return "一样大";}
-    else{
-        return b;}
+        return a;
+    } else if (a == b){
+        return "一样大";
+    } else{
+        return b;
+    }
 }
 document.write(" 5 和 4 的较大值是:"+ max(5, 4) + "<br>");
 document.write(" 6 和 3 的较大值是:" + max(6, 3));
